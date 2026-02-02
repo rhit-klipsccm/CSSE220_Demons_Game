25a8f0514577661b9bc41d34e548db5d8bebe952
rhit-hans <ZHANGZ36@rose-hulman.edu>
2026-02-01 21:05:23 -0500

    interface sprite

--- Demons_Game/src/Game/Sprites.java
+++ Demons_Game/src/Game/Sprites.java
@@ -1,5 +1,8 @@
 package Game;
 
-public interface Sprites {
+import java.awt.Graphics2D;
 
+public interface Sprites {
+    void draw(Graphics2D g2);// this is for painting the frame
+    void setAction(String actionName); // this is for changing the motion like walk and dead
 }
